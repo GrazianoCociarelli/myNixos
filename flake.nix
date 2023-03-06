83ff8bc9c8a73e801ca8ca0a99fe318afc1f284e
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-06 22:59:37 +0800

    update

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      hostname = nixpkgs.lib.nixosSystem {
+      "kvm" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./system/configuration.nix
