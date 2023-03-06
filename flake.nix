c7d0ea7e2bde86f424ef833c91fcef8ac1d92144
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-06 22:58:41 +0800

    update

--- flake.nix
+++ flake.nix
@@ -17,8 +17,6 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.jdoe = import ./home.nix;
-
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
