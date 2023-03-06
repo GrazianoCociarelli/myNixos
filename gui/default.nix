65478c0f2f4dd5da74a963658c0a3dfa141e0019
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-06 21:53:41 +0800

    update

--- gui/default.nix
+++ gui/default.nix
@@ -7,5 +7,13 @@
         ];
       });
     })
+
+     (self: super: {
+      slstatus = super.slstatus.overrideAttrs (oldAttrs: rec {
+        patches =  [
+          ./mystatus.patch
+        ];
+      });
+    })
   ];
 }
