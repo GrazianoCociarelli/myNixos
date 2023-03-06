7dfa86233daaf987a03355d03c9ee8f2f9230e77
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-06 22:27:51 +0800

    update

--- flake.nix
+++ flake.nix
@@ -19,6 +19,13 @@
         inherit system;
         modules = [
           ./system/configuration.nix
+        
+          inputs.home-manager.nixosModules.home-manager {
+            nixpkgs.config.allowUnfree = true;
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit username; };
+          }
         ];
       };
     };
