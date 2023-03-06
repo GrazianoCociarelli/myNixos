427df869d347c6071b92ef66940d7e3c3bf063d5
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-06 23:15:27 +0800

    update

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.wwt = import ./home.nix;
-            # home-manager.users.wwt.isNormalUser = true;
+            home-manager.users.wwt.isNormalUser = true;
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
