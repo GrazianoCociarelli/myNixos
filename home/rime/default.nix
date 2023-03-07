0dde3b2147f324a47b05c9ca8603e2066d734b8b
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-08 00:11:12 +0800

    ha

--- home/rime/default.nix
+++ home/rime/default.nix
@@ -1,11 +1,6 @@
 {
-  home.file = {
-    ".config/ibus/rime/default.custom.yaml".source = ./default.custom.yaml;
-    ".config/ibus/rime/build/double_pinyin.schema.yaml".source = ./double_pinyin.schema.yaml;
-    ".config/ibus/rime/build/double_pinyin_plus.custom.yaml".source = ./double_pinyin_plus.custom.yaml;
-    ".config/ibus/rime/build/double_pinyin_plus.schema.yaml".source = ./double_pinyin_plus.schema.yaml;
-    ".config/ibus/rime/build/zrm2000.dict.yaml".source = ./zrm2000.dict.yaml;
-    ".config/ibus/rime/build/zrm2000.schema.yaml".source = ./zrm2000.schema.yaml;
-    ".config/ibus/rime/build/luna_pinyin.dict.yaml".source = ./luna_pinyin.dict.yaml
+  home.file.".config/ibus/rime" = {
+    source = ./asserts
+    recursive = true;
   };
 }
