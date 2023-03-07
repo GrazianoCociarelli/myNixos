42539b81a7ac12aca41731c05dd93e9404fae85b
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-07 19:23:26 +0800

    update

--- system/configuration.nix
+++ system/configuration.nix
@@ -47,6 +47,7 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.inputMethod.enabled = "fcitx5";
+  i18n.inputMethod.fcitx5.enableRimeData = true;
   i18n.inputMethod.fcitx5.addons = with pkgs; [
     fcitx5-chinese-addons
     fcitx5-rime
