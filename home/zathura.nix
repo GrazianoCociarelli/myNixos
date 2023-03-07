736cd1e7d1a897a5a5264d7f4013d8da59e7f055
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-07 10:14:21 +0800

    update

---
+++ home/zathura.nix
@@ -0,0 +1,16 @@
+{
+  programs.zathura = {
+    enable = true;
+
+    options = {
+      font = "monospace normal 12";
+      recolor = true;
+      recolor-darkcolor      = "#C5C8C6";
+      recolor-lightcolor     = "#1D1F21";
+      statusbar-fg           = "#C5C8C6";
+      statusbar-bg           = "#1D1F21";
+      statusbar-basename     = true;
+      statusbar-page-percent = true;
+    };
+  };
+}
