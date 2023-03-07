10401ee67c4855ef5308154138e92f320db67f80
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-07 13:28:11 +0800

    color

--- home/default.nix
+++ home/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./startx.nix
     ./alacritty.nix
+    ./qutebrowser.nix
     ./zathura.nix
   ];
   home.stateVersion = "22.11";
--- home/zathura.nix
+++ home/zathura.nix
@@ -9,6 +9,7 @@
       recolor-lightcolor     = "#1D1F21";
       statusbar-fg           = "#C5C8C6";
       statusbar-bg           = "#1D1F21";
+      default-bg           = "#1D1F21";
       statusbar-basename     = true;
       statusbar-page-percent = true;
     };
