b982e9b565df24e1d1ea286cd9e4d2d032b5ea78
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-07 23:49:47 +0800

    update

--- home/rime/default.nix
+++ home/rime/default.nix
@@ -4,5 +4,7 @@
     ".config/ibus/rime/build/double_pinyin.schema.yaml".source = ./double_pinyin.schema.yaml;
     ".config/ibus/rime/build/double_pinyin_plus.custom.yaml".source = ./double_pinyin_plus.custom.yaml;
     ".config/ibus/rime/build/double_pinyin_plus.schema.yaml".source = ./double_pinyin_plus.schema.yaml;
+    ".config/ibus/rime/build/zrm2000.dict.yaml".source = ./zrm2000.dict.yaml;
+    ".config/ibus/rime/build/zrm2000.schema.yaml".source = ./zrm2000.schema.yaml;
   };
 }
