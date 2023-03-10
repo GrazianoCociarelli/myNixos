3a74334ae6ff4d1ebd8c8825069bb2dfcf3c2d30
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-10 09:46:53 +0800

    update

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -22,7 +22,6 @@
           let g:tex_flavor = 'latex'
           let g:vimtex_view_method = 'zathura'
           let g:vomtex_quickfix_mode = 0
-          set encoding = utf-8
         '';
       }
 
