5113f193a4daa65c6d71dde5cb29be1066361484
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-10 09:44:58 +0800

    update

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -4,12 +4,37 @@
     enable = true;
     vimAlias = true;
     plugins = with pkgs.vimPlugins; [
+
       vim-nix
-      vim-latex-live-preview
-      vimtex
+      
+      {
+        plugin = vim-latex-live-preview;
+        config = ''
+          let g:livepreview_previewer = 'zathura'
+          let g:livepreview_engine = 'pdflatex'
+          autocmd Filetype tex setl updatetime=500
+        '';
+      }
+
+      {
+        plugin = vimtex;
+        config = ''
+          let g:tex_flavor = 'latex'
+          let g:vimtex_view_method = 'zathura'
+          let g:vomtex_quickfix_mode = 0
+          set encoding = utf-8
+        '';
+      }
+
       ultisnips
 
-      nvim-base16
+      {
+        plugin = nvim-base16;
+        config = ''
+          colorscheme base16-tomorrow-night
+        '';
+      }
+
     ];
   };
 }
