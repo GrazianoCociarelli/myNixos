b2ac2ad710bba8efa00c9d160d2c02c3be9b0154
grazianocociarelli <grazianocociarelli@proton.me>
2023-03-28 22:52:02 +0800

    update

--- home/qutebrowser/default.nix
+++ home/qutebrowser/default.nix
@@ -1,4 +1,4 @@
 { 
   programs.qutebrowser.enable = true;
-  home.file.".config/qutebrwser/config.py".source = ./base16-tomorrow-night.config.py;
+  home.file.".config/qutebrowser/config.py".source = ./base16-tomorrow-night.config.py;
 }
